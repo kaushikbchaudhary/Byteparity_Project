fc52a54ee0689a17aa5c15cc205c6c43005ebef6
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-08-29 18:08:06 +0530

    Amazon website page clone using html and css

---
+++ Amazon Clone/script.js
@@ -0,0 +1,7 @@
+//
+// const searchBorder = document.querySelector(".nav-search-field");
+// const searchfield = document.querySelector(".search-input");
+//
+// searchfield.addEventListener("click",function (){
+//     searchBorder.style.border = '3px solid #f3a109';
+// });
