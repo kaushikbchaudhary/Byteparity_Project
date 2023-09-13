2d9a58b473a83b089cca4511aba526af89505638
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-09-13 11:30:15 +0530

    asmr Project update

--- asmr/index.js
+++ asmr/index.js
@@ -1,3 +1,14 @@
- // orders.forEach(order => {
- //
- // })
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.productStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'Primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+        
+    console.log("Hello world");
+})
