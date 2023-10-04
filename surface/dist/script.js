c3d6302bc10572967495611203b590f1a6d59da5
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-10-04 14:39:06 +0530

    New Project

---
+++ surface/dist/script.js
@@ -0,0 +1,32 @@
+'use strict!';
+
+const tabContainer = document.querySelector(".tabs");
+const integrationSection = document.querySelector(".integrations");
+const tabContentSiblings = document.querySelector(".tab__content").parentElement.children;
+// console.log(tabContentSiblings.);
+
+// console.log(integrationSection.children);
+// console.log(integrationSection.childNodes);
+
+const siblingAnchors = document.querySelectorAll('.tabs ul li a');
+console.log(siblingAnchors);
+tabContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'A') {
+        console.log(e.target.dataset.tab);
+        const tabClicked = e.target.dataset.tab;
+       
+        // const siblingAnchors = Array.from(e.target.parentElement.parentElement.children).map(li => li.querySelector('a'));
+        // console.log(siblingAnchors);
+        // siblingAnchors.forEach(a => {
+        //     console.log(a);
+        //     a.classList.remove('activeTab');
+        // });
+        
+        siblingAnchors.forEach(anchor => anchor.classList.remove('activeTab'));
+        e.target.classList.add('activeTab');
+        console.log(e.target.classList);
+        document.querySelectorAll('.tab__content').forEach(tab => tab.classList.remove('activeTabContent'));
+        document.querySelector(`.${tabClicked}`).classList.add('activeTabContent');
+
+    }
+})
